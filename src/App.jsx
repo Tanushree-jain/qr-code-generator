b0361ae9739b8ed2e5f816fe37b9f300fdf689e8
Tanushree-jain <tanushreegangwal19@gmail.com>
2025-04-25 10:22:33 +0530

    footer css changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import QRCodeStyling from "qr-code-styling";
 import { ColorPicker, useColor } from "react-color-palette";
 import "react-color-palette/css";
 import "./App.css";
-import logo from "./assets/logo.png"; 
+import logo from "./assets/logo.png";
 
 
 function App() {
@@ -120,10 +120,10 @@ function App() {
             dgColorPickerRef.current &&
             !dgColorPickerRef.current.contains(event.target) &&
             !event.target.closest(".color-dot-wrapper"))
-            (showColorPicker === "sg" &&
-              sgColorPickerRef.current &&
-              !sgColorPickerRef.current.contains(event.target) &&
-              !event.target.closest(".color-dot-wrapper"))
+          (showColorPicker === "sg" &&
+            sgColorPickerRef.current &&
+            !sgColorPickerRef.current.contains(event.target) &&
+            !event.target.closest(".color-dot-wrapper"))
       ) {
         setShowColorPicker(null);
       }
@@ -160,223 +160,225 @@ function App() {
       qrRef.current.innerHTML = "";
       qrCodeInstance.append(qrRef.current);
     }
-  }, [qrCode, fgColor, bgColor,dgColor,sgColor, qrSize, logoFile]);
+  }, [qrCode, fgColor, bgColor, dgColor, sgColor, qrSize, logoFile]);
 
   return (
     <>
-    <div className="App">
-    
-      <div className="qr-studio">
-        <img src={logo} alt="logo" className="logo" />
-        <input
-          type="text"
-          value={inputValue}
-          onChange={handleChange}
-          placeholder="Enter URL, text or contact details"
-        />
-        <button onClick={handleGenerateQRCode}>Generate QR Code</button>
+      <div className="App">
 
-            {qrCode ? (
-              <>
-                <div ref={qrRef} className="qr-code-container"></div>
-                <button onClick={handleDownloadQRCode} className="download">Download QR Code</button>
-              </>
-            ) : (
-          <p>Enter text and generate a QR code</p>
-        )}
-      </div>
-      <div className="edit-panel">
-        <h2>Design Your QR</h2>
-        <h3
-          className="collapsible-header"
-          onClick={() => setShowColorSection(!showColorSection)}
-        >
-          Color <span className="direction">{showColorSection ? "▲" : "▼"}</span>
-        </h3>
-        {showColorSection && (
-          <div className="color-section">
-            <div className="color-dots-container">
-              <div className="color-dot-wrapper" onClick={() => toggleColorPicker("fg")}>
-                <div
-                  className="color-dot"
-                  style={{ backgroundColor: fgColor.hex }}
-                  title="Foreground Color"
-                />
-                <span>Foreground</span>
-              </div>
-             
-              <div className="color-dot-wrapper" onClick={() => toggleColorPicker("bg")}>
-                <div
-                  className="color-dot"
-                  style={{ backgroundColor: bgColor.hex }}
-                  title="Background Color"
-                />
-                <span>Background</span>
-              </div>
-              <div className="color-dot-wrapper" onClick={() => toggleColorPicker("dg")}>
-                <div
-                  className="color-dot"
-                  style={{ backgroundColor: dgColor.hex }}
-                  title="Dot Corner"
-                />
-                <span>Dot Corner</span>
-              </div>
-              <div className="color-dot-wrapper" onClick={() => toggleColorPicker("sg")}>
-                <div
-                  className="color-dot"
-                  style={{ backgroundColor: sgColor.hex }}
-                  title="Square Corner"
-                />
-                <span>Square Corner</span>
-              </div>
-            </div>
-            {showColorPicker === "fg" && (
-              <div className="color-picker-popup" ref={fgColorPickerRef}>
-                <ColorPicker
-                  width={100}
-                  height={80}
-                  color={fgColor}
-                  onChange={setFgColor}
-                  hideHSV
-                  dark
-                />
-              </div>
-            )}
-            {showColorPicker === "bg" && (
-              <div className="color-picker-popup" ref={bgColorPickerRef}>
-                <ColorPicker
-                  width={100}
-                  height={80}
-                  color={bgColor}
-                  onChange={setBgColor}
-                  dark
-                />
-              </div>
-            )}
-              {showColorPicker === "dg" && (
-              <div className="color-picker-popup" ref={dgColorPickerRef}>
-                <ColorPicker
-                  width={100}
-                  height={80}
-                  color={dgColor}
-                  onChange={setDgColor}
-                  dark
-                />
-              </div>
-            )}
-             {showColorPicker === "sg" && (
-              <div className="color-picker-popup" ref={sgColorPickerRef}>
-                <ColorPicker
-                  width={100}
-                  height={80}
-                  color={sgColor}
-                  onChange={setSgColor}
-                  dark
-                />
-              </div>
-            )}
-          </div>
-        )}
-        <h3
-          className="collapsible-header"
-          onClick={() => setShowDotsSection(!showDotsSection)}
-        >
-          Dots & Corners<span className="direction">{showDotsSection ? "▲" : "▼"}</span>
-        </h3>
-        {showDotsSection && (
-          <div className="color-section">
-            <span>Dot Type</span>
-            <select
-              value={dotShape}
-              onChange={(e) => {
-                const selectedShape = e.target.value;
-                setDotShape(selectedShape);
-                qrCodeInstance.update({
-                  dotsOptions: {
-                    type: selectedShape,
-                  },
-                });
-              }}
-            >
-              <option value="rounded">Rounded</option>
-              <option value="dots">Dots</option>
-              <option value="square">Square</option>
-              <option value="classy">Classy</option>
-              <option value="extra-rounded">Extra Rounded</option>
-              <option value="classy-rounded">Classy Rounded</option>
-            </select>
-
-            <span>Corners Dot</span>
-            <select
-              value={cornerDotShape}
-              onChange={(e) => {
-                const selectedShape = e.target.value;
-                setCornerDotShape(selectedShape);
-                qrCodeInstance.update({
-                  cornersDotOptions: {
-                    type: selectedShape,
-                  },
-                });
-              }}
-            >
-              <option value="dot">Dot</option>
-              <option value="square">Square</option>
-            </select>
-
-            <span>Corners Square</span>
-            <select
-              value={cornerSquareShape}
-              onChange={(e) => {
-                const selectedShape = e.target.value;
-                setCornerSquareShape(selectedShape);
-                qrCodeInstance.update({
-                  cornersSquareOptions: {
-                    type: selectedShape,
-                  },
-                });
-              }}
-            >
-              <option value="dot">Dot</option>
-              <option value="square">Square</option>
-              <option value="extra-rounded">Extra Rounded</option>
-            </select>
-          </div>
-
-          
-        )}
-        <div className="size-input-container">
-          <label htmlFor="qrSizeInput" className="size-input-label">
-            Size
-          </label>
+        <div className="qr-studio">
+          <img src={logo} alt="logo" className="logo" />
           <input
-  id="qrSizeInput"
-  type="number"
-  min={128}
-  max={1024}
-  value={qrSize}
-  onChange={(e) => {
-    let val = parseInt(e.target.value);
-    if (isNaN(val)) val = 256;
-    if (val < 128) val = 128;
-    if (val > 1024) val = 1024;
-    setQrSize(val);
-  }}
-  className="size-input-field"
-  placeholder="256"
-/>
+            type="text"
+            value={inputValue}
+            onChange={handleChange}
+            placeholder="Enter URL, text or contact details"
+          />
+          <button onClick={handleGenerateQRCode}>Generate QR Code</button>
+
+          {qrCode ? (
+            <>
+              <div ref={qrRef} className="qr-code-container"></div>
+              <button onClick={handleDownloadQRCode} className="download">Download QR Code</button>
+            </>
+          ) : (
+            <p>Enter text and generate a QR code</p>
+          )}
+        </div>
+        <div className="edit-panel">
+          <h2>Design Your QR</h2>
+          <div className="img-input-container">
+            <label htmlFor="qrlogoInput" className="size-input-label">
+              Upload Logo
+            </label>
+            <input type="file" accept="image/*" onChange={handleLogoUpload} />
+          </div>
+          <h3
+            className="collapsible-header"
+            onClick={() => setShowColorSection(!showColorSection)}
+          >
+            Color  & Size<span className="direction">{showColorSection ? "▲" : "▼"}</span>
+          </h3>
+          {showColorSection && (
+            <div className="color-section">
+              <div className="color-dots-container">
+                <div className="color-dot-wrapper" onClick={() => toggleColorPicker("fg")}>
+                  <div
+                    className="color-dot"
+                    style={{ backgroundColor: fgColor.hex }}
+                    title="Foreground Color"
+                  />
+                  <span>Foreground</span>
+                </div>
+
+                <div className="color-dot-wrapper" onClick={() => toggleColorPicker("bg")}>
+                  <div
+                    className="color-dot"
+                    style={{ backgroundColor: bgColor.hex }}
+                    title="Background Color"
+                  />
+                  <span>Background</span>
+                </div>
+                <div className="color-dot-wrapper" onClick={() => toggleColorPicker("dg")}>
+                  <div
+                    className="color-dot"
+                    style={{ backgroundColor: dgColor.hex }}
+                    title="Dot Corner"
+                  />
+                  <span>Dot Corner</span>
+                </div>
+                <div className="color-dot-wrapper" onClick={() => toggleColorPicker("sg")}>
+                  <div
+                    className="color-dot"
+                    style={{ backgroundColor: sgColor.hex }}
+                    title="Square Corner"
+                  />
+                  <span>Square Corner</span>
+                </div>
+              </div>
+              {showColorPicker === "fg" && (
+                <div className="color-picker-popup" ref={fgColorPickerRef}>
+                  <ColorPicker
+                    width={100}
+                    height={80}
+                    color={fgColor}
+                    onChange={setFgColor}
+                    hideHSV
+                    dark
+                  />
+                </div>
+              )}
+              {showColorPicker === "bg" && (
+                <div className="color-picker-popup" ref={bgColorPickerRef}>
+                  <ColorPicker
+                    width={100}
+                    height={80}
+                    color={bgColor}
+                    onChange={setBgColor}
+                    dark
+                  />
+                </div>
+              )}
+              {showColorPicker === "dg" && (
+                <div className="color-picker-popup" ref={dgColorPickerRef}>
+                  <ColorPicker
+                    width={100}
+                    height={80}
+                    color={dgColor}
+                    onChange={setDgColor}
+                    dark
+                  />
+                </div>
+              )}
+              {showColorPicker === "sg" && (
+                <div className="color-picker-popup" ref={sgColorPickerRef}>
+                  <ColorPicker
+                    width={100}
+                    height={80}
+                    color={sgColor}
+                    onChange={setSgColor}
+                    dark
+                  />
+                </div>
+              )}
+               <div className="size-input-container">
+            <label htmlFor="qrSizeInput" className="size-input-label">
+              Size
+            </label>
+            <input
+              id="qrSizeInput"
+              type="number"
+              min={128}
+              max={1024}
+              value={qrSize}
+              onChange={(e) => {
+                let val = parseInt(e.target.value);
+                if (isNaN(val)) val = 256;
+                if (val < 128) val = 128;
+                if (val > 1024) val = 1024;
+                setQrSize(val);
+              }}
+              className="size-input-field"
+              placeholder="256"
+            />
+
+          </div>
+            </div>
+          )}
+          <h3
+            className="collapsible-header"
+            onClick={() => setShowDotsSection(!showDotsSection)}
+          >
+            Dots & Corners<span className="direction">{showDotsSection ? "▲" : "▼"}</span>
+          </h3>
+          {showDotsSection && (
+            <div className="color-section">
+              <span>Dot Type</span>
+              <select
+                value={dotShape}
+                onChange={(e) => {
+                  const selectedShape = e.target.value;
+                  setDotShape(selectedShape);
+                  qrCodeInstance.update({
+                    dotsOptions: {
+                      type: selectedShape,
+                    },
+                  });
+                }}
+              >
+                <option value="rounded">Rounded</option>
+                <option value="dots">Dots</option>
+                <option value="square">Square</option>
+                <option value="classy">Classy</option>
+                <option value="extra-rounded">Extra Rounded</option>
+                <option value="classy-rounded">Classy Rounded</option>
+              </select>
+
+              <span>Corners Dot</span>
+              <select
+                value={cornerDotShape}
+                onChange={(e) => {
+                  const selectedShape = e.target.value;
+                  setCornerDotShape(selectedShape);
+                  qrCodeInstance.update({
+                    cornersDotOptions: {
+                      type: selectedShape,
+                    },
+                  });
+                }}
+              >
+                <option value="dot">Dot</option>
+                <option value="square">Square</option>
+              </select>
+
+              <span>Corners Square</span>
+              <select
+                value={cornerSquareShape}
+                onChange={(e) => {
+                  const selectedShape = e.target.value;
+                  setCornerSquareShape(selectedShape);
+                  qrCodeInstance.update({
+                    cornersSquareOptions: {
+                      type: selectedShape,
+                    },
+                  });
+                }}
+              >
+                <option value="dot">Dot</option>
+                <option value="square">Square</option>
+                <option value="extra-rounded">Extra Rounded</option>
+              </select>
+            </div>
+
+
+          )}
+         
+
 
         </div>
 
-        <div className="img-input-container">
-          <label htmlFor="qrlogoInput" className="size-input-label">
-            Upload Logo
-          </label>
-          <input type="file" accept="image/*" onChange={handleLogoUpload} />
-        </div>
       </div>
-
-    </div>
-    <footer className="footer">Made with ❤️ by Tanushree | <a href="https://github.com/Tanushree-jain">GitHub</a> | <a href="https://www.linkedin.com/in/tanushree-gangwal">LinkedIn</a></footer>
+      <footer className="footer">The QR spot – crafted by Tanushree with ❤️ | <a href="https://github.com/Tanushree-jain">GitHub</a> | <a href="https://www.linkedin.com/in/tanushree-gangwal">LinkedIn</a></footer>
     </>
   );
 }
